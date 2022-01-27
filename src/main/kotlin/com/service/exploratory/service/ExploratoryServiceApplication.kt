28c6ede672d91ea0b3d3c1947aefcc77bdfdd372
Shitiz Khanna <shitizkhanna2@gmail.com>
2022-01-27 17:00:55 +0530

    initial commit

---
+++ src/main/kotlin/com/service/exploratory/service/ExploratoryServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.service.exploratory.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExploratoryServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExploratoryServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/service/exploratory/service/ExploratoryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.service.exploratory.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExploratoryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
